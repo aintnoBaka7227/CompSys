6decc30c6e4212bcbceddab9af4889e0fd2f7235
aintnoBaka7227 <phongtrando2004@gmail.com>
2024-09-11 21:00:21 +0930

    not sure why it still buggy

--- prac5/Mult/Mult.asm
+++ prac5/Mult/Mult.asm
@@ -1,3 +1,4 @@
+
 // This file is based on part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
@@ -7,50 +8,49 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
-@R2
-D=M
-// check if R2 = 0 then set R0 to 0
-@ZERO
-D;JEQ
-// check if R2 is positive (using R2 for loop)
-@POSITIVE
-D;JGT
-// if not then negate R2
-D=!D
-D=D+1
-(POSITIVE)
-@i
-M=D
-// loop start from R2, end when i = 0
+    @1
+    D=M
+    @2
+    D=M 
+    @3
+    M=1
+    @NEGATIVE
+    D;JLT
+    @ZERO
+    D;JEQ
+    @LOOP
+    D;JGE
+(NEGATIVE)
+    @3
+    M=-1
+    @2
+    M=!M
+    M=M+1
 (LOOP)
-@R1
-D=M
-@R0
-M=M+D
-@i
-MD=M-1
-@LOOP
-D;JGT
+    @R1
+    D=M
+    @R0
+    M=M+D
+    @2
+    MD=M-1
+    @LOOP
+    D;JGT
 
-// check again for R2 if it is negative then negate R0
-@R2
-D=M
-@NEGATE
-D;JLT
-
-(END)
-@END
-0;JMP
-
-(NEGATE)
-@R0
-M=!M
-M=M+1
-@END
-0;JMP
-
+    @3
+    D=M
+    @NEGATION
+    D;JLT
+    @END
+    D;JGE
+(NEGATION)
+    @0
+    M=!M
+    M=M+1
+    @END
+    0;JMP
 (ZERO)
-@R0
-M=0
-@END
-0;JMP
+    @0
+    M=0
+(END)
+    @END
+    0;JMP
--- prac5/Mult/MultNew.asm
+++ prac5/Mult/MultNew.asm
@@ -1,3 +1,8 @@
+
+
+
+
+    
 // This file is based on part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
@@ -7,9 +12,6 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
-
-@i
-i=0
 @R2
 D=M
 // check if R2 = 0 then set R0 to 0
@@ -21,22 +23,19 @@ D;JGT
 // if not then negate R2
 D=!D
 D=D+1
-@counter
-M=D
 (POSITIVE)
-// loop start from 0, end when i = R2
+@i
+M=D
+// loop start from R2, end when i = 0
 (LOOP)
 @R1
 D=M
 @R0
 M=M+D
 @i
-M=M+1
-D=M
-@counter
-D=D-M
+MD=M-1
 @LOOP
-D;JLT
+D;JGT
 
 // check again for R2 if it is negative then negate R0
 @R2
@@ -60,78 +59,4 @@ M=M+1
 M=0
 @END
 0;JMP
-
-
-// This file is based on part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: Mult.asm
-
-// Multiplies R1 and R2 and stores the result in R0.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-
-// Put your code here.
-    @i
-    M=1
-    @1
-    D=M
-    @4
-    M=D
-    @2
-    D=M 
-    @3
-    M=1
-    @NEGATIVE
-    D;JLT
-    @ZERO
-    D;JEQ
-    @LOOP
-    D;JGE
-(NEGATIVE)
-    @3
-    M=-1
-    @2
-    M=!M
-    M=M+1
-(LOOP)
-    @i
-    D=M
-    @2
-    D=D-M
-    @WRITE
-    D;JGE
-    @4
-    D=M
-    @1
-    M=D+M
-    @i
-    M=M+1
-    @LOOP
-    0;JMP
-(WRITE)
-    @1
-    D=M
-    @0
-    M=D
-    @3
-    D=M
-    @NEGATION
-    D;JLT
-    @END
-    D;JGE
-(NEGATION)
-    @1
-    D=M
-    D=!D
-    D=D+1
-    @0
-    M=D
-    @END
-    0;JMP
-(ZERO)
-    @0
-    M=0
-(END)
-    @END
-    0;JMP
 
