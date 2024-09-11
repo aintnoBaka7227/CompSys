9aeb34b7302ec675789f429c44f2beebe6319bc8
aintnoBaka7227 <phongtrando2004@gmail.com>
2024-09-11 20:09:57 +0930

    rewrite code base for better readability

--- prac5/Mult/Mult.asm
+++ prac5/Mult/Mult.asm
@@ -7,66 +7,54 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
-    @i
-    M=1
-    @1
-    D=M
-    @4
-    M=D
-    @2
-    D=M 
-    @3
-    M=1
-    @NEGATIVE
-    D;JLT
-    @ZERO
-    D;JEQ
-    @LOOP
-    D;JGE
-(NEGATIVE)
-    @3
-    M=-1
-    @2
-    M=!M
-    M=M+1
+@counter
+M=1
+@R2
+D=M
+// check if R2 = 0 then set R0 to 0
+@ZERO
+D;JEQ
+// check if R2 is positive (using R2 for loop)
+@POSITIVE
+D;JGT
+// if not then negate R2
+D=!D
+D=D+1
+@i
+M=D
+(POSITIVE)
+// loop start from 0, end when i = R2
 (LOOP)
-    @i
-    D=M
-    @2
-    D=D-M
-    @WRITE
-    D;JGE
-    @4
-    D=M
-    @1
-    M=D+M
-    @i
-    M=M+1
-    @LOOP
-    0;JMP
-(WRITE)
-    @1
-    D=M
-    @0
-    M=D
-    @3
-    D=M
-    @NEGATION
-    D;JLT
-    @END
-    D;JGE
-(NEGATION)
-    @1
-    D=M
-    D=!D
-    D=D+1
-    @0
-    M=D
-    @END
-    0;JMP
-(ZERO)
-    @0
-    M=0
+@R1
+D=M
+@R0
+M=M+D
+@counter
+MD=M+1
+@i
+D=D-M
+@LOOP
+D;JLE
+
+// check again for R2 if it is negative then negate R0
+@R2
+D=M
+@NEGATE
+D;JLT
+
 (END)
-    @END
-    0;JMP
+@END
+0;JMP
+
+(NEGATE)
+@R0
+M=!M
+M=M+1
+@END
+0;JMP
+
+(ZERO)
+@R0
+M=0
+@END
+0;JMP
